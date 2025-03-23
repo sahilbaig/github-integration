e44c42406381dd573bc90e5a1042ae051593638a
Sahil Baig <sahilbaig.me@gmail.com>
2025-03-24 01:51:06 +0530

    testing input fro webhook

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express()
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.json({ "message": "success" })
+
+})
+
+app.post('/webhook', (req, res) => {
+    // Log the event type and payload
+    console.log('Webhook received!');
+    console.log('Event:', req.headers['x-github-event']);
+    console.log('Payload:', req.body);
+
+    // Send a success response to GitHub
+    res.status(200).send('Webhook received');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
